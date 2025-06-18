ab4a91c67d2a460e52f7b6206a11e44bf58ba6ff
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-06-18 20:05:26 +0800

    .

--- client/index.js
+++ client/index.js
@@ -3,8 +3,8 @@ export default (ctx) => {
     // 此 Context 非彼 Context
     // 我们只是在前端同样实现了一套插件逻辑
     ctx.page({
-        name: '页面标题',
-        path: '/custom-page',
+        name: '',
+        path: '/chatluna-preset-editor',
         component: Page,
     });
 };
